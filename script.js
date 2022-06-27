fa5782cbc4a26e34cf99debd47d1e12c26d06cc3
leowilson <leobca2018@gmail.com>
2022-06-27 13:01:56 +0530

    add html and script files.

---
+++ script.js
@@ -0,0 +1,10 @@
+var numberRange = parseInt(prompt());
+console.log('Range : ' , numberRange);
+for ( i = 1; i <= numberRange; i++){
+    if ( i % 2 == 0){
+        console.log(i);
+    }
+    
+}
+
+
